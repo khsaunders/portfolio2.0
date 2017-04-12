0977b71ad64923012e03be37c70f2299c76cd107
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-04-12 13:00:33 -0400

    Fix header spacing by adding padding to top of image carousel, setting top margin to 0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 console.log('connected');
+
+$(document).ready(function(){
+  $('.carousel').slick({
+/*  slidesToShow: 3,
+  slidesToScroll: 1,*/
+  autoplay: true,
+  autoplaySpeed: 2500
+ });
+});
